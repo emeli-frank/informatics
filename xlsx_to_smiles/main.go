6ba4d54c4b4d50304d617eb1c9b797f98bc8d279
Frank Emeli <dev.carlfrank@gmail.com>
2020-03-14 10:41:06 +0100

    xsls_to_smilies.go not provides useful info to users

--- xlsx_to_smiles/main.go
+++ xlsx_to_smiles/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
 	"io/ioutil"
@@ -8,29 +9,51 @@ import (
 )
 
 func main() {
+	showHelpMsg()
+
 	f, err := excelize.OpenFile("smiles.xlsx")
 	check(err)
 
-	os.MkdirAll("./smiles", os.ModePerm)
+	err = os.MkdirAll("./smiles", os.ModePerm)
+	check(err)
 
-	// Get value from cell by given worksheet name and axis.
 	for i := 1; ; i++ {
 		name := f.GetCellValue("Sheet1", fmt.Sprintf("A%d", i))
 		smi := f.GetCellValue("Sheet1", fmt.Sprintf("B%d", i))
-		if smi == "" {
+		if smi == "" || name == "" {
 			break
 		}
 
 		err := ioutil.WriteFile(fmt.Sprintf("./smiles/%s.smi", name), []byte(smi), 0644)
 		check(err)
 
-		//fmt.Println(i, "\t",  name, "\t", smi)
+		fmt.Println(i, "\t",  name, "\t", smi)
 	}
+
+	fmt.Print("DONE!:", "Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
 
 
 func check(e error) {
 	if e != nil {
 		panic(e)
+		fmt.Print("Press 'Enter' to continue...")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
 	}
+}
+
+func showHelpMsg() {
+	fmt.Println("Make sure the following rules are met:")
+	fmt.Printf("\t1, Your excel file is named 'smiles'\n")
+	fmt.Printf("\t2, The data you want to extract is in a sheet called 'Sheet1'\n")
+	fmt.Printf("\t3, The first and second column of your sheet are the names and the smi strings respectively'\n")
+	fmt.Printf("\t4, There are no empty rows between data rows. The program assumes an empty row is the end of data\n")
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+	fmt.Println("The smi files will be created in a directory named 'smiles'")
+	fmt.Println("Any conflicting names will be overwritten")
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
